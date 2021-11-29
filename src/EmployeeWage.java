3960641602c3015b6de2f5e2c2f20f5da1ea7a0b
Makrand <makrandshingare@gmail.com>
2021-11-29 12:16:10 +0530

    Added Switch case isntade of if-else stmt..

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,39 +1,42 @@
 public class EmployeeWage {
     static int status;
-    static int RATE_PER_HOUR ;
-    static int wage=0;
-    static int PART_TIME;
-    static int FULL_TIME;
+    static int RATE_PER_HOUR;
+    static int wage = 0;
+    final static int PART_TIME = 0;
+    final static int FULL_TIME = 1;
     static int emphours;
+
     EmployeeWage() {
-        RATE_PER_HOUR=20;
-        PART_TIME=2;
-        FULL_TIME=1;
+        RATE_PER_HOUR = 20;
     }
+
     static int isPresent() {
-        status=(int)(Math.random()*10)%3;
-        if(status==FULL_TIME)
-            emphours=8;
-        else if(status==PART_TIME)
-            emphours=4;
-        else
-            emphours=0;
+        status = (int) (Math.random() * 10) % 3;
+        switch (status) {
+            case FULL_TIME:
+                emphours = 8;
+                break;
+            case PART_TIME:
+                emphours = 4;
+                break;
+            default:
+                emphours = 0;
+        }
         return emphours;
     }
 
     public static void main(String[] args) {
 
-        EmployeeWage employeeWage=new EmployeeWage();
+        EmployeeWage employeeWage = new EmployeeWage();
 
-        if((isPresent())>0){
-            calculateWage();
-        }
+        if ((isPresent()) > 0) calculateWage();
         else System.out.println("Employee is absent");
-        System.out.println("Total Wage of Employee is : "+employeeWage.wage);
+
+        System.out.println("Total Wage of Employee is : " + employeeWage.wage);
     }
 
     static void calculateWage() {
-        wage=emphours*RATE_PER_HOUR;
+        wage = emphours * RATE_PER_HOUR;
     }
 
 }
